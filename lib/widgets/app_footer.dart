11beb4dd23f287c39d4f7afc52eac3fe43ab71d7
Adorjan Rieck <adorjan.rieck@icloud.com>
2025-09-30 17:01:16 +0200

    Bottom Bar better slide to left/right

--- lib/widgets/app_footer.dart
+++ lib/widgets/app_footer.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import '../features/level_map/level_map_screen.dart';
 import '../features/shop/shop_screen.dart';
 import '../features/leaderboard/leaderboard_screen.dart';
@@ -10,6 +9,53 @@ class AppFooter extends StatelessWidget {
   final FooterTab activeTab;
 
   const AppFooter({super.key, required this.activeTab});
+
+  void _navigate(BuildContext context, FooterTab targetTab) {
+    if (targetTab == activeTab) return;
+
+    Widget targetScreen;
+    switch (targetTab) {
+      case FooterTab.shop:
+        targetScreen = const ShopScreen();
+        break;
+      case FooterTab.map:
+        targetScreen = const LevelMapScreen();
+        break;
+      case FooterTab.leaderboard:
+        targetScreen = const LeaderboardScreen();
+        break;
+    }
+
+    // Reihenfolge bestimmen
+    final tabOrder = [FooterTab.shop, FooterTab.map, FooterTab.leaderboard];
+    final currentIndex = tabOrder.indexOf(activeTab);
+    final targetIndex = tabOrder.indexOf(targetTab);
+
+    // Richtung bestimmen
+    final isForward = targetIndex > currentIndex;
+
+    // Wenn „überspringen“ (z.B. Shop -> Leaderboard), Animation schneller
+    final skip = (currentIndex - targetIndex).abs() > 1;
+
+    Navigator.pushReplacement(
+      context,
+      PageRouteBuilder(
+        transitionDuration: Duration(milliseconds: skip ? 150 : 300),
+        pageBuilder: (_, __, ___) => targetScreen,
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          final begin = Offset(isForward ? 1.0 : -1.0, 0.0);
+          const end = Offset.zero;
+          final tween = Tween(begin: begin, end: end)
+              .chain(CurveTween(curve: Curves.easeInOut));
+
+          return SlideTransition(
+            position: animation.drive(tween),
+            child: child,
+          );
+        },
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,16 +68,7 @@ class AppFooter extends StatelessWidget {
           // Shop
           Expanded(
             child: InkWell(
-              onTap: activeTab == FooterTab.shop
-                  ? null
-                  : () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (_) => const ShopScreen(),
-                  ),
-                );
-              },
+              onTap: () => _navigate(context, FooterTab.shop),
               child: Center(
                 child: Text(
                   "Shop",
@@ -49,16 +86,7 @@ class AppFooter extends StatelessWidget {
           // Map
           Expanded(
             child: InkWell(
-              onTap: activeTab == FooterTab.map
-                  ? null
-                  : () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (_) => const LevelMapScreen(),
-                  ),
-                );
-              },
+              onTap: () => _navigate(context, FooterTab.map),
               child: Center(
                 child: Text(
                   "Map",
@@ -76,16 +104,7 @@ class AppFooter extends StatelessWidget {
           // Leaderboard
           Expanded(
             child: InkWell(
-              onTap: activeTab == FooterTab.leaderboard
-                  ? null
-                  : () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (_) => const LeaderboardScreen(),
-                  ),
-                );
-              },
+              onTap: () => _navigate(context, FooterTab.leaderboard),
               child: Center(
                 child: Text(
                   "Leaderboard",
